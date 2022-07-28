facf16f2a4b745ebea1a56b7c863bfeb7686974d
Atharva <atharvakutwal2002@gmail.com>
2022-07-28 21:33:46 +0530

    separate modules added

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      res.status(400);
+      res.send(error);
+    });
+});
+
+app.get('./users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catched((e)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    const _id= req.params.id
+    User.findById(_id).then((user)=>{
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch(()=>{
+        res.status(500).send()
+    })
+})
+
+app.post("./tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+  console.log("server is running on port " + port);
+});
---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const validator = require('validator')
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password must not contain word 'password '");
+      }
+      // if(!validator.isPassword(value)){
+      //   throw new Error("Not a valid password")
+      // }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
+
+module.exports= User;
