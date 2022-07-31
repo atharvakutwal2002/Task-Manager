f46b10ea6007f9dd183a090e8b76b9f76ea030b1
Atharva <atharvakutwal2002@gmail.com>
2022-07-31 20:54:51 +0530

    another endpoint added

--- src/index.js
+++ src/index.js
@@ -30,6 +30,26 @@ app.get('./users',(req,res)=>{
     })
 })
 
+app.get('/tasks',(req, res)=>{
+  Task.find({}).then((tasks)=>{
+    res.send(tasks)
+  }).catch((e)=>{ 
+    res.send(500).send()
+  })
+})
+
+app.get('/tasks/:id' ,(req,res)=>{
+  const _id=req.params.id
+  Task.findById(_id).then((task)=>{
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  }).catch((e)=>{
+    res.status(500).send()
+  })
+})
+
 app.get('/users/:id',(req,res)=>{
     const _id= req.params.id
     User.findById(_id).then((user)=>{
